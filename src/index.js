44e70554e94c3b3706e75716592896c6fba80375
olkuska <olkysha10@gmail.com>
2021-08-19 21:34:05 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+  if (arguments.length == 0 || array.length == 0) {
+    return 0;
+  }
+    let minNumber = Math.min.apply(null, array)
+    for ( i = 0; i< array.length; i++) {
+      if ( array[i] < minNumber) {
+        minNumber = array[i]
+      } 
+    }
+   return minNumber
+  }
 
 exports.max = function max (array) {
-  return 0;
+  if (arguments.length == 0 || array.length == 0) {
+    return 0;
+  }  
+  let maxNumber = Math.max.apply(null, array)
+  for ( i = 0; i< array.length; i++) {
+    if ( array[i] > maxNumber) {
+      maxNumber = array[i]
+    } 
+  }
+ return maxNumber
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (arguments.length == 0 || array.length == 0) {
+    return 0;
+  } 
+  let sum = array.reduce(function(a, b) {return a+b})
+
+  for ( i = 0; i< array.length; i++) {
+   let length = array.length;  
+   return result = sum / length;
+  }
 }
